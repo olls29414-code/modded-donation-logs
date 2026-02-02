60d0d484041dcbe0076068528935960886b1ba8f
olls29414-code <olls29414@gmail.com>
2026-02-02 22:32:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -59,4 +59,8 @@ app.post("/generate", async (req, res) => {
   res.json({ ok: true });
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+
